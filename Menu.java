c525309784b7ce701756ac2c2c3db0acb835af29
João Lucas Freitas <joao.frhb@gmail.com>
2022-05-29 17:42:07 -0300

    adicionando novo menu

--- Menu.java
+++ Menu.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class Menu{
     public static void main(String[] args) {
-            System.out.println("MENU DE OPÇÕES:    ");  
+            System.out.println("MENU DE OPÇÕES:    ");
+            System.out.println("MENU DE OPÇÕES 222:    ");  
             
             int opcao;
             do{
