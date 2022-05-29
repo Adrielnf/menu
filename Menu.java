2d69a6680b4bee6b64d4429457c9630b4e223e26
Adriel Amaral <adrielsilvanf61@gmail.com>
2022-05-29 17:29:23 -0300

    Primeiro commit

---
+++ Menu.java
@@ -0,0 +1,33 @@
+import java.lang.ref.Cleaner;
+import java.util.Scanner;
+
+public class Menu{
+    public static void main(String[] args) {
+            System.out.println("MENU DE OPÇÕES:    ");  
+            
+            int opcao;
+            do{
+                System.out.println("1. Opção 1          ");
+                System.out.println("2. Opção 2          ");
+                System.out.println("3. Sair             ");
+                System.out.print("Digite a opção desejada:");
+                
+                Scanner scanner = new Scanner(System.in);
+                opcao = scanner.nextInt();
+
+                switch (opcao){
+                    case 1:
+                        System.out.println("Você escolheu a primeira opção");
+                    break; 
+                    case 2:
+                        System.out.println("Você escolheu a segunda opção");
+                    break;
+                    default:
+                    System.out.println("O programa foi encerrado");
+                opcao = 0;
+            }
+        } while (opcao!= 0);  
+        
+    }
+
+}
